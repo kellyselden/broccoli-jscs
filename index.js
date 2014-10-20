fd851db6db95ac9a66b9c02c39a14d920922a369
Kelly Selden <kellyselden@gmail.com>
2014-10-20 13:10:26 -0400

    first version

---
+++ index.js
@@ -0,0 +1,28 @@
+var Filter = require('broccoli-filter');
+var jscs = require('jscs');
+
+var JscsFilter = function(inputTree, options) {
+  if (!(this instanceof JscsFilter)) return new JscsFilter(inputTree, options);
+
+  this.inputTree = inputTree;
+
+  var checker = new jscs({esnext: options && !!options.esnext});
+  checker.registerDefaultRules();
+  checker.configure(config.load(options.configPath));
+  this.checker = checker;
+};
+
+JscsFilter.prototype = Object.create(Filter.prototype);
+JscsFilter.prototype.constructor = JscsFilter;
+JscsFilter.prototype.extensions = ['js'];
+JscsFilter.prototype.targetExtension = 'js';
+JscsFilter.prototype.processString = function(content, relativePath) {
+  var errors = this.checker.checkString(content, relativePath);
+  errors.getErrorList().forEach(function (err) {
+    console.log(errors.explainError(err, true));
+  });
+
+  return content;
+};
+
+module.exports = JscsFilter;
