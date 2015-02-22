50f65586b622ab04dc082226fd94ce690ef14370
Robert Jackson <robert.w.jackson@me.com>
2015-02-22 16:40:15 -0500

    Use lintTree hook for ember-cli 0.1.10 and above.
    This prevents us from linting an addon's `app/` or `test-support/` tree.

--- ember-addon-main.js
+++ ember-addon-main.js
@@ -1,22 +1,38 @@
 var JSCSFilter = require('broccoli-jscs');
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles  = require('broccoli-static-compiler');
+var checker = require('ember-cli-version-checker');
 
 module.exports = {
   name: 'broccoli-jscs',
 
+  shouldSetupRegistryInIncluded: function() {
+    return !checker.isAbove(this, '0.1.10');
+  },
+
+  lintTree: function(type, tree) {
+    var jscsTree = new JSCSFilter(tree, this.app.options.jscsOptions);
+
+    if (!jscsTree.enabled || jscsTree.bypass || jscsTree.disableTestGenerator) {
+      return;
+    }
+
+    return jscsTree;
+  },
+
   included: function(app) {
+    var addonContext = this;
     this.app = app;
     this._super.included.apply(this, arguments);
 
-    if (app.tests) {
+    if (app.tests && this.shouldSetupRegistryInIncluded()) {
       app.registry.add('js', {
         name: 'broccoli-jscs',
         ext: 'js',
         toTree: function(tree, inputPath, outputPath, options) {
-          var jscsTree = new JSCSFilter(tree, app.options.jscsOptions);
+          var jscsTree = addonContext.lintTree('unknown-type', tree);
 
-          if (!jscsTree.enabled || jscsTree.bypass || jscsTree.disableTestGenerator) {
+          if (!jscsTree) {
             return tree;
           }
 
